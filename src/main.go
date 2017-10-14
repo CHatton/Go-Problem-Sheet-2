7541143d3e47b1b4da5113482506c244097dd4f0
chatton <cianhatton@gmail.com>
2017-10-14 21:05:04 +0100

    added setting cookie if the cookie didn't exist

--- src/main.go
+++ src/main.go
@@ -4,9 +4,11 @@ import (
 	"./message"
 	"fmt"
 	"html/template"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -16,18 +18,33 @@ func main() {
 	// on how to server specifc html pages. Not just index in the specified folder.
 	http.HandleFunc("/guess/", func(w http.ResponseWriter, r *http.Request) {
 
+		if !hasCookies(r) {
+			// generate a new number between 1 - 20
+			// I consulted this article https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/06.1.html
+			// to learn how to set cookies in the response
+			rand.Seed(time.Now().UTC().UnixNano())             // seed rand so we get different values each time
+			randomNum := rand.Intn(20) + 1                     // number between 1 - 20
+			expiration := time.Now().Add(365 * 24 * time.Hour) // expires in 1 year
+			cookie := http.Cookie{Name: "target", Value: strconv.Itoa(randomNum), Expires: expiration}
+			http.SetCookie(w, &cookie)
+		} // otherwise leave "target" at the current value
+
 		// create the template from the template file.
 		guessTemplate := template.Must( // program will throw an error if there is a problem here
 			template.ParseFiles("./html/guess.tmpl"))
-
 		guessTemplate.Execute(w, msg)
 	})
 	// serves index.html in the res folder.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
 		http.ServeFile(w, r, "./html/index.html")
 	})
 
 	http.ListenAndServe(":"+port, nil)
+}
+
+func hasCookies(r *http.Request) bool {
+	return len(r.Cookies()) != 0
 }
 
 func getPort() string {
